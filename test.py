c961f23d9575b541c342246542c5d5f787d97a02
mckset <99543950+mckset@users.noreply.github.com>
2022-02-17 20:57:38 -0500

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-# first of all import the socket library
-import socket            
- 
-# next create a socket object
-s = socket.socket()        
-s.connect(("127.0.0.1", 6969))
-s.send("blacklist".encode())
